7aa7c3cfd34fb8c91c03154c4a94223c10837bfa
chris-smekens-vinci <chris.smekens@student.vinci.be>
2024-11-01 14:27:23 +0100

    new new:ex2.1

--- exercices/2.1-2-3-4/src/App.tsx
+++ exercices/2.1-2-3-4/src/App.tsx
@@ -6,19 +6,16 @@ import './App.css'
 
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
-
   const cinema1Name = "UGC DeBrouckère";
   const cinema1Movie1Title = "Film 1 - DeBrouckère";
   const cinema1Movie1Director = "Director A";
   const cinema1Movie2Title = "Film 2 - DeBrouckère";
   const cinema1Movie2Director = "Director B";
-
   const cinema2Name = "UGC Toison d'Or";
   const cinema2Movie1Title = "Film 1 - Toison d'Or";
   const cinema2Movie1Director = "Director C";
   const cinema2Movie2Title = "Film 2 - Toison d'Or";
   const cinema2Movie2Director = "Director D";
-
   return (
     <div>
       <PageTitle title={pageTitle} />
@@ -41,11 +38,9 @@ const App = () => {
     </div>
   );
 };
-
 const PageTitle = (props: { title: string }) => {
   return <h1>{props.title}</h1>;
 };
-
 const Cinema = (props: { 
   name: string,
   movie1Title: string,
@@ -68,5 +63,4 @@ const Cinema = (props: {
     </div>
 );
 
-export default App;
-
+export default App
