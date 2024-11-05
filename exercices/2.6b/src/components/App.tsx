49cd74cb3f15c22c72087a2990fd1fb0db3a7549
chris-smekens-vinci <chris.smekens@student.vinci.be>
2024-11-05 11:32:59 +0100

    new:ex2.6b

---
+++ exercices/2.6b/src/components/App.tsx
@@ -0,0 +1,13 @@
+import ColorBox from "./ColorBox";
+
+// j'ai pas codé, j'ia juste CP/COLLER le code ts.
+
+const App = () => (
+  <>
+    <ColorBox />
+    <ColorBox />
+    <ColorBox />
+  </>
+);
+
+export default App;
