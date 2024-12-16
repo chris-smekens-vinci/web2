c0f49dd191b10384df795f89bea2533eda508fea
chris-smekens-vinci <chris.smekens@student.vinci.be>
2024-12-16 13:27:45 +0100

    new:ex2.14

--- exercices/2.13-14/src/App.tsx
+++ exercices/2.13-14/src/App.tsx
@@ -14,6 +14,9 @@ const App: React.FC = () => {
 
   useEffect(() => {
     fetchJoke(); // Appelle fetchJoke() lors du premier rendu du composant. Le code à l'intérieur de useEffect est une fonction qui sera exécutée après que le composant soit rendu.
+
+    const interval = setInterval(fetchJoke, 10000); // Fetch a new joke every 10 seconds
+    return () => clearInterval(interval); // Cleanup interval on component unmount
   }, []); // Le tableau vide [] en second argument signifie que cet effet ne s'exécutera qu'une seule fois, après le premier rendu du composant.
 
   // Si joke est "truthy" (existe, non vide, non null, non undefined), alors la première partie est exécutée. Sinon, la seconde partie est exécutée.
@@ -22,6 +25,11 @@ const App: React.FC = () => {
       <h1>Random Joke</h1>
       {joke ? <Joke joke={joke} /> : <p>Loading...</p>}
       <button className="reload-button" onClick={fetchJoke}>Reload Joke</button>
+      <br />
+      <br />
+      <p>
+        <cite>https://v2.jokeapi.dev/joke.category</cite>
+      </p>
     </div>
   );
 };
