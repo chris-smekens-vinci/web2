349cc8a16743d55c2f8fd9efa208c14b11c89e32
chris-smekens-vinci <chris.smekens@student.vinci.be>
2024-12-16 12:35:52 +0100

    new:ex2.13b

---
+++ exercices/2.13b/src/components/RandomDog.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+const RandomDog: React.FC = () => {
+  const [dogImage, setDogImage] = useState<string>('');
+
+  const fetchDogImage = async () => {
+    try {
+      const response = await fetch('https://dog.ceo/api/breeds/image/random');
+      const data = await response.json();
+      setDogImage(data.message);
+    } catch (error) {
+      console.error('Error fetching dog image:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDogImage();
+  }, []);
+
+  return (
+    <div>
+      {dogImage ? <img src={dogImage} alt="Random Dog" /> : <p>Loading...</p>}
+    </div>
+  );
+};
+
+export default RandomDog;
