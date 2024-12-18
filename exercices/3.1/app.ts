1c2bfc129067dae62f913114c40ee9174103010d
chris-smekens-vinci <chris.smekens@student.vinci.be>
2024-12-18 14:35:53 +0100

    new:ex3.1

---
+++ exercices/3.1/app.ts
@@ -0,0 +1,40 @@
+import express, { ErrorRequestHandler } from "express";
+import cors from "cors";
+
+import usersRouter from "./routes/users";
+import authsRouter from "./routes/auths";
+import filmsRouter from "./routes/films";
+
+
+const app = express();
+
+const corsOptions = {
+  origin: [/^http:\/\/localhost/, "http://amazing.you.com"],
+};
+
+app.use(cors(corsOptions));
+
+app.use((_req, _res, next) => {
+  console.log(
+    "Time:",
+    new Date().toLocaleString("fr-FR", { timeZone: "Europe/Brussels" })
+  );
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/users", usersRouter);
+app.use("/auths", authsRouter);
+app.use("/films", filmsRouter);
+
+
+const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+  return;
+};
+
+app.use(errorHandler);
+export default app;
---
+++ exercices/3.1/types.ts
@@ -0,0 +1,46 @@
+import { Request } from "express";
+
+interface AuthenticatedUser {
+  username: string;
+  token: string;
+}
+
+interface User {
+  id: number;
+  username: string;
+  password: string;
+}
+
+type PotentialUser = Omit<User, "id">;
+
+interface AuthenticatedRequest extends Request {
+  user?: User;
+}
+
+interface JwtPayload {
+  username: string;
+  exp: number; // Expiration time (in seconds since the epoch)
+  iat: number; // Issued at time (in seconds since the epoch)
+}
+
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  urlImage?: string;
+}
+
+type NewFilm = Omit<Film, "id">;
+
+export type {
+  AuthenticatedUser,
+  User,
+  PotentialUser,
+  AuthenticatedRequest,
+  JwtPayload,
+  Film,
+  NewFilm,
+};
